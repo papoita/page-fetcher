5435cdbc75fa73a9da7fff4839dda9a003c1f5e5
PaolaP <paolaperez8@gmail.com>
2021-11-19 22:34:37 -0500

    prints to file and logs byte size

--- fetcher.js
+++ fetcher.js
@@ -1,12 +1,13 @@
 /** @format */
 const request = require("request");
+const fs = require("fs");
 request("http://www.example.edu/", (error, response, body) => {
 	console.log("error:", error); // Print the error if one occurred
+
 	console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
 	console.log("body:", body); // Print the HTML for the Google homepage.
-	//   import { writeFile } from "fs";
-	//   import { Buffer } from "buffer";
-	// const request = require("request");
-
-	// 	writeFile("message.txt", "Hello Node.js", "utf8", callback);
+	fs.writeFile("bodyReq.html", body, "utf8", request);
+	// (err) => {
+	// 	if (err) throw err;
+	console.log(`Downloaded and saved ${body.length} bytes to ./bodyReq.txt`);
 });
