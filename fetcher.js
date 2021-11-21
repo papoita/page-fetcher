f950956b8fa1f1099c033750e11d2fe1f038a0cf
PaolaP <paolaperez8@gmail.com>
2021-11-20 23:55:36 -0500

    use input for url and path from CLI

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,25 @@
 /** @format */
 // /**
-//  * @TODO to request path and URL thorugh the CLI using process.argv.slice
+//  * @TODO there is still an unhandled error event
+//var args = process.argv.slice(2);
 //  */
 const request = require("request");
 const fs = require("fs");
+const urlInput = process.argv[2];
+const path = process.argv[3];
 
-request("http://www.example.edu/", (error, response, body) => {
-  if (error) {
-    console.log("error:", error);
-  }
-  // Print the error if one occurred
+request(urlInput, (error, response, body) => {
+	console.log("error:", error);
+	// Print the error if one occurred
+	console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+	console.log("body:", body); // Print the HTML for the Google homepage.
 
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
-  fs.writeFile("bodyReq.html", body, "utf8", request);
-  (err) => {
-    if (err) throw err;
-    console.log(`Downloaded and saved ${body.length} bytes to ./bodyReq.txt`);
-  };
+	if (!error) {
+		fs.writeFile(path, body, "utf8", request, (err) => {
+			if (err) throw err;
+			console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+		});
+	}
 });
 
-//
+//in CLI< node fetcher.js http://www.example.edu/>
