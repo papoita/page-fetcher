d00f6fbe0bad3406e23b3e6e42a948f6e0ae0a0d
PaolaP <paolaperez8@gmail.com>
2021-11-22 06:03:28 -0500

    success code

--- fetcher.js
+++ fetcher.js
@@ -9,17 +9,17 @@ const urlInput = process.argv[2];
 const path = process.argv[3];
 
 request(urlInput, (error, response, body) => {
-  console.log("error:", error);
-  // Print the error if one occurred
-  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-  console.log("body:", body); // Print the HTML for the Google homepage.
+	console.log("error:", error);
+	// Print the error if one occurred
+	console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+	console.log("body:", body); // Print the HTML for the Google homepage.
 
-  if (!error) {
-    fs.writeFile(path, body, "utf8", request, (err) => {
-      if (err) throw err;
-      console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
-    });
-  }
+	if (!error) {
+		fs.writeFile(path, body, "utf8", (err) => {
+			if (err) throw err;
+			console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+		});
+	}
 });
 
-//in CLI< node fetcher.js http://www.example.edu/> ./index.html
+//in CLI< node fetcher.js http://www.example.edu ./index.html
